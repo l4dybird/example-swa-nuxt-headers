2288a0df5b335bfb873c99b2c139cae1197a95a7
l4dybird <future.l4dy61rd@gmail.com>
2024-11-25 14:25:00 +0900

    :fire: routeRules を削除する

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ export default defineNuxtConfig({
   experimental: {
     typedPages: true,
   },
-  routeRules: {
-    '/a/**': { cache: { maxAge: 60 * 5 } },
-  },
   compatibilityDate: '2024-11-01',
   ssr: false,
   css: ['~/assets/main.css'],
